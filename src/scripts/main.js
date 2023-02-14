0736867c591e9207c4b0502aeaa7a817cbd9a6dd
Gustavo Almeida <gustavo.almei2@hotmail.com>
2023-02-14 09:30:24 -0300

    fix countdown

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,18 +7,20 @@ let contaHora = setInterval(function() {
     const agora = new Date();
     const tempoAtual = agora.getTime();
     const distanciaEvento = tempoEvento - tempoAtual;
-    const mesEmMs = 1000 * 60 * 60 * 24 * 30;
-    const diaEmMs = 1000 * 60 * 60 * 24;
-    const horaEmMs = 1000 * 60 * 60;
-    const minEmMs = 1000 * 60;
-    const segEmMs = 1000;
-    const mesAteEvento = Math.floor(distanciaEvento / mesEmMs)
-    const diasAteEvento = Math.floor((distanciaEvento % mesEmMs)/diaEmMs)
-    const horasAteEvento = Math.floor((distanciaEvento % diaEmMs)/horaEmMs)
-    const minAteEvento = Math.floor((distanciaEvento % horaEmMs)/minEmMs)
-    const segAteEvento = Math.floor((distanciaEvento % minEmMs)/segEmMs)
+    const calcTime = {
+        diaEmMs : 1000 * 60 * 60 * 24,
+        horaEmMs : 1000 * 60 * 60,
+        minEmMs : 1000 * 60,
+        segEmMs : 1000
+    }
+    const timeCont = {
+        diasAteEvento : Math.floor(distanciaEvento / calcTime.diaEmMs),
+        horasAteEvento : Math.floor((distanciaEvento % calcTime.diaEmMs)/calcTime.horaEmMs),
+        minAteEvento : Math.floor((distanciaEvento % calcTime.horaEmMs)/calcTime.minEmMs),
+        segAteEvento : Math.floor((distanciaEvento % calcTime.minEmMs)/calcTime.segEmMs)
+    }
 
-    document.querySelector('#contador').textContent = `${mesAteEvento}m ${diasAteEvento}d ${horasAteEvento}h ${minAteEvento}m ${segAteEvento}s`
+    document.querySelector('#contador').textContent = `${timeCont.diasAteEvento}d ${timeCont.horasAteEvento}h ${timeCont.minAteEvento}m ${timeCont.segAteEvento}s`
 
     if( distanciaEvento < 0){
         clearInterval(contaHora);
